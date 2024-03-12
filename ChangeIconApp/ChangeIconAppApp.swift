befaee6153523b3ebe356665951f6c0d10a4e5e6
Yevhen Zelinskyi <3elinskiy@gmail.com>
2024-03-12 14:49:19 +0200

    Initial Commit

---
+++ ChangeIconApp/ChangeIconAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChangeIconAppApp.swift
+//  ChangeIconApp
+//
+//  Created by Evgeniy Zelinskiy on 12.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct ChangeIconAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
